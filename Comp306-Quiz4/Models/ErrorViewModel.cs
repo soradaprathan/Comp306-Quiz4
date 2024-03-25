24bd865eb4cef7d964e9c6ad470f14670d675ec3
Sorada Prathan <sprathan@my.centennialcollege.ca>
2024-03-25 09:07:14 -0400

    Add project files.

---
+++ Comp306-Quiz4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Comp306_Quiz4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
